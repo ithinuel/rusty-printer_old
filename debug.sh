36646463819aab587a82479cbcfd69f72a83930e
Wilfried Chauveau <ithinuel@gmail.com>
2018-04-14 22:55:23 +0100

    add binary & hex image generation

--- debug.sh
+++ debug.sh
@@ -1,1 +1,5 @@
-cargo clean && cargo build --target=thumbv7m-none-eabi && arm-none-eabi-objdump -DSC target/thumbv7m-none-eabi/debug/rusty_bootloader | less
+cargo clean && \
+    cargo build --target=thumbv7m-none-eabi && \
+    arm-none-eabi-objcopy -O binary target/thumbv7m-none-eabi/debug/rusty_bootloader target/thumbv7m-none-eabi/debug/rusty_bootloader.bin && \
+    arm-none-eabi-objcopy -O ihex target/thumbv7m-none-eabi/debug/rusty_bootloader target/thumbv7m-none-eabi/debug/rusty_bootloader.hex && \
+    arm-none-eabi-objdump -DSC target/thumbv7m-none-eabi/debug/rusty_bootloader | less
